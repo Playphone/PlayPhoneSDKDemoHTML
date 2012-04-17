bd7b5b2f9e39c4637e5cba1820f7238cc33d1a79
M.Chuhryaev <maximc@playphone.com>
2012-04-17 17:13:29 +0300

    Welcome notification logic added for logged-in users

--- inc/mod/API_Init.php
+++ inc/mod/API_Init.php
@@ -20,6 +20,9 @@ class API_InitModule extends Module
      MNDirect.AppReloadClient.APP_AFTER_RELOAD_AUTO
     );
 
+    // Init welcome notification for logged in users
+    MNDirectPopup.init (MNDirectPopup.MNDIRECTPOPUP_WELCOME);
+
     // Required for init SDK parameters definition
     var GAME_ID = <?= SDK_GAME_ID ?>;
     var GAME_SECRET = <?= SDK_GAME_SECRET ?>;
